529aa7c83df410bbc6b89d33b8c8b7b33f2b9863
MooseMoose <moosemoosesgaming@gmail.com>
2017-12-11 23:18:25 +0100

    first commit

---
+++ UnityProject/ArenaSurvival/Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public float pitch = 2f;
+    private float currentZoom = 10f; 
+
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+
+    public Vector2 panLimit;
+    public float scrollSpeed = 20f;
+    public float minY = 20f;
+    public float maxY = 120f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if(Input.GetKey("up") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("down") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("right") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("left") || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * 100f * scrollSpeed * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, +panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, +panLimit.y);
+
+        transform.position = pos;
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            transform.position = target.position - offset * currentZoom;
+            transform.LookAt(target.position + Vector3.up * pitch);
+        }
+
+    }
+
+}
