18e2df81c33496c0de4a8df044ea3aadc3585793
MooseMoose <moosemoosesgaming@gmail.com>
2017-12-17 00:20:51 +0100

    Interact hopefully done

--- UnityProject/ArenaSurvival/Assets/Scripts/Units/Interactable.cs
+++ UnityProject/ArenaSurvival/Assets/Scripts/Units/Interactable.cs
@@ -3,18 +3,29 @@
 public class Interactable : MonoBehaviour {
 
     public float radius = 3f;
+    public Transform interactionTransform;
 
     bool isFocus = false;
     Transform player;
 
+    bool hasInteracted = false;
+
+    public virtual void Interact ()
+    {
+        //This method is ment to be overwritten
+        Debug.Log("Interacting with " + transform.name);
+    }
+
     void Update ()
     {
-        if (isFocus)
+        if (isFocus && !hasInteracted)
         {
-            float distance = Vector3.Distance(player.position, transform.position);
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
             if(distance <= radius)
             {
-                Debug.Log("ITERACT");
+                //Debug.Log("ITERACT"); // Remove to remove log message
+                hasInteracted = true;
+                Interact();
             }
         }
     }
@@ -23,18 +34,20 @@ public class Interactable : MonoBehaviour {
     {
         isFocus = true;
         player = playerTransform;
+        hasInteracted = false;
     }
 
     public void OnDefocused()
     {
         isFocus = false;
         player = null;
+        hasInteracted = false;
     }
 
     void OnDrawGizmosSelected()
     {
 
         Gizmos.color = new Color(1, 1, 0, 0.75F);
-        Gizmos.DrawWireSphere(transform.position, radius);
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
     }
 }
