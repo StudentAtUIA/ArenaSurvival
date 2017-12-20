733999ee46c4da3d0b6af1f7cdf13882837a137d
MooseMoose <moosemoosesgaming@gmail.com>
2017-12-20 18:36:40 +0100

    Added simple menus

---
+++ UnityProject/ArenaSurvival/Assets/Scripts/GUIs/InGameMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour {
+
+    public Canvas gameMenu;
+    private bool escPressed = false;
+
+    void Start()
+    {
+        gameMenu.enabled = false; 
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Escape) && escPressed == false)
+        {
+            gameMenu.enabled = true;
+            escPressed = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && escPressed == true)
+        {
+            gameMenu.enabled = false;
+            escPressed = false;
+        }
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
